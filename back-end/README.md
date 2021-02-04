9140762de1e3e0a02d48047a3939c2a839e4f6d8
Natane <natane-b@hotmail.fr>
2021-02-04 14:54:53 +0100

    Upload et download des fichier sur S3

--- back-end/README.md
+++ back-end/README.md
@@ -1,1 +1,21 @@
 # aws-project-esgi-2021
+
+
+## Installer les dépendances 
+```
+pip install -r requirements.txt
+```
+## Configurer la BDD
+ 
+- Créer une base de donnée : `awsdbdjango`
+
+- Lancer les migrations :
+
+        python manage.py migrate
+
+
+# Start project
+
+Pour lancer le serveur web depuis le terminal:
+yil so
+cett    python manage.py runserver
--- back-end/aws_back_end/aws_api/views.py
+++ back-end/aws_back_end/aws_api/views.py
@@ -1,11 +1,14 @@
 import json
-
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Matiere
+import boto
+import boto3
 
-
+AWS_ACCESS_KEY = 'AKIAXL4VZJYZTK6AVZ4P'
+AWS_SECRET_KEY = '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'
+AWS_BUCKET_NAME = 'aws-project-esgi-test'
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -40,5 +43,42 @@ def add_matiere_rds(request):
 
     data = {
         "succes": succes
+    }
+    return JsonResponse(data)
+
+def download_file_s3(request):
+    conn = boto.connect_s3(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    bucket = conn.get_bucket(AWS_BUCKET_NAME)
+    bucket_list = bucket.list()
+    file_name_and_url_s3 = []
+    for l in bucket_list:
+        keyString = str(l.key)
+        s3_url = f"https://{AWS_BUCKET_NAME}.s3.amazonaws.com/{keyString}"
+        url_data = {
+            "name": keyString,
+            "url": s3_url
+        }
+        file_name_and_url_s3.append(url_data)
+
+    data = {
+        "succes": True,
+        "file": file_name_and_url_s3
+    }
+    return JsonResponse(data)
+
+@csrf_exempt
+def upload_file_s3(request):
+    conn = boto3.session.Session(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    s3_resource = conn.resource('s3')
+    bucket = s3_resource.Bucket(AWS_BUCKET_NAME)
+
+    if request.method == 'POST' and request.FILES['file']:
+        myfile = request.FILES['file']
+        file_name = myfile.name
+        bucket.put_object(Key=file_name, Body=myfile.read(), ACL='public-read')
+
+    data = {
+        "succes": True,
+        "file": f"https://{AWS_BUCKET_NAME}.s3.amazonaws.com/{file_name}"
     }
     return JsonResponse(data)
